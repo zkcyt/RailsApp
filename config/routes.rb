889ee86716b83dd2d7657717801b7f8604bd23a9
zkcyt <tmsinksz@gmail.com>
2018-07-22 15:58:59 +0900

    cards completed reading cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards', to: 'cards#index'
+
+  get 'cards/add'
+  post 'cards/add'
+
+  get 'cards/:id', to: 'cards#show'
+
+  get 'cards/edit/:id', to: 'cards#edit'
+  patch 'cards/edit/:id', to: 'cards#edit'
+
+  get 'cards/delete/:id', to: 'cards#delete'
+
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'helo/index'
   get 'helo', to: 'helo#index'
-  #get 'helo/other'
-
   post 'helo', to: 'helo#index'
   post 'helo/index'
 end
